0c35a20d32619b47752963afc9648175412685f1
Pavel Tyukin <me@patyukin.ru>
2024-12-10 22:57:49 +0300

    add migrations

--- migrations/20241112141330_init.sql
+++ migrations/20241112141330_init.sql
@@ -13,35 +13,5 @@ CREATE TABLE auth_audit_log
       PARTITION BY event_date
       ORDER BY (database, schema, table, event_time);
 
-CREATE TABLE payments_audit_log
-(
-    database   String,
-    schema     String,
-    table      String,
-    operation  String,
-    event_time DateTime64(3, 'UTC'),
-    data       String,
-    event_date Date,
-    created_at Datetime
-) ENGINE = MergeTree()
-      PARTITION BY event_date
-      ORDER BY (database, schema, table, event_time);
-
-CREATE TABLE credits_audit_log
-(
-    database   String,
-    schema     String,
-    table      String,
-    operation  String,
-    event_time DateTime64(3, 'UTC'),
-    data       String,
-    event_date Date,
-    created_at Datetime
-) ENGINE = MergeTree()
-      PARTITION BY event_date
-      ORDER BY (database, schema, table, event_time);
-
 -- +goose Down
 DROP TABLE IF EXISTS auth_audit_log;
-DROP TABLE IF EXISTS payments_audit_log;
-DROP TABLE IF EXISTS credits_audit_log;
